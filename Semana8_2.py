9fddfcc5f8b8432125af9435feacc2017ce5d15e
Rose T. Álvarez <i1610546@continental.edu.pe>
2024-10-31 22:31:27 -0500

    Correcciones de vista

--- Semana8_2.py
+++ Semana8_2.py
@@ -2,23 +2,29 @@ import streamlit as st
 
 def suma(a, b):
     return a + b
+
 def resta(a, b):
     return a - b
+
 def multiplicacion(a,b):
     return a * b
+
 def division(a,b):
-    if b != 0:
+    if b != 0: # ! es dintinto de (si es distinto a cero)
         return a/b
     else:
         return "Error: División por cero"
 
 def main():
     st.title("Calculadora Básica")
+
     #Entrada de datos
     numero1 = st.number_input("Ingrese el primer número:", format="%f")
     numero2 = st.number_input("Ingrese el segundo número:", format="%f")
+
     #Selección de operación
     operacion = st.selectbox("Seleccione la operación", ("Suma", "Resta", "Multiplicación", "División"))
+
     #Realizar la operación seleccionada
     if operacion == "Suma":
         resultado = suma(numero1, numero2)
@@ -28,7 +34,8 @@ def main():
         resultado = multiplicacion(numero1, numero2)
     elif operacion == "División":
         resultado = division(numero1, numero2)
-    #monstrar el resultado
+
+    #Mostrar el resultado
     st.write(f"Resultado: {resultado}")
 
 if __name__ == "__main__":
