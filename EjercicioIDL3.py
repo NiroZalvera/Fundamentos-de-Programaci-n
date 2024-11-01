590cc1547105be2b22c8f2b9e825f2271b3c99c2
Rose T. Álvarez <i1610546@continental.edu.pe>
2024-11-01 15:11:46 -0500

    a ver

--- EjercicioIDL3.py
+++ EjercicioIDL3.py
@@ -1,11 +1,20 @@
 import streamlit as st
 
- suma=0
- cont=0
- num=int(input("Número:"))
- while num!=0:
- 	cont=cont+1
- 	suma=suma+num
- 	num=int(input("Número:"))
- media=float(suma)/cont
- print("La suma es %d y la media es %f"%(suma,media))
+def marca()
+def modelo()
+def kilometraje()
+    if kilometraje < 0:
+         st.write("Por favor, ingrese un radio mayor a cero.")
+
+def main():
+    st.title("Datos del Automóvil")
+
+    marca = st.text_input("Ingrese la marca del automóvil: ")
+    modelo = st.text_input("Ingrese el modelo del automóvil: ")
+    kilometraje = st.number_input("Ingrese el kilometraje del automóvil: ")
+    
+    #Mostrar el resultado
+    st.write(f"La marca es: {marca}, el modelo es: {modelo}, el kilometraje es: {kilometraje}")
+
+if __name__ == "__main__":
+    main()
